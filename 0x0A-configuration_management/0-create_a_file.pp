6966330f3b08d32690d3b9de47165adfc4bc42ae
Von-sledge <papaamoah568@gmail.com>
2024-04-06 07:48:32 +0000

    Add task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Filename: 0-create_a_file.pp
 
 file { '/tmp/school':
-	ensure	=> file,
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love puppet',
-	}
+        ensure  => file,
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love puppet',
+        }
