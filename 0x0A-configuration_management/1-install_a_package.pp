992ac3f32c6eb946999b9c0aef8216579e6d498e
Von-sledge <papaamoah568@gmail.com>
2024-04-06 08:26:24 +0000

    Add Task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Install flask from pip3 using puppet.
+
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+}
