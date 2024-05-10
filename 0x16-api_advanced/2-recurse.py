c46c91bb7a5b609ecff5ee0898e0067582736f53
Futuristic-Dev <papaamoah568@gmail.com>
2024-05-10 04:16:05 +0000

    Add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,9 +4,8 @@
 Write a recursive function that queries the Reddit API.
 
 Returns a list containing the titles of all hot articles for a given subreddit.
-If no results are found for the given subreddit, the function should\n
+If no results are found for the given subreddit, the function should.
 Return None.
-
 Hint: The Reddit API uses pagination for separating pages of responses.
 """
 
@@ -19,7 +18,7 @@ def recurse(subreddit, hot_list=[], after=''):
     """
     Query the Reddit API recursively.
 
-    Returns a list containing the titles of all hot articles\n
+    Returns a list containing the titles of all hot articles.
     For a given subreddit.
     """
     if subreddit is None or not isinstance(subreddit, str):
@@ -30,8 +29,7 @@ def recurse(subreddit, hot_list=[], after=''):
         'User-Agent': 'api_advanced-project'
     }
     params = {'after': after}
-    results = requests.get(url, params=params, headers=headers, 
-    allow_redirects=False)
+    results = requests.get(url, params=params, headers=headers)
 
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
